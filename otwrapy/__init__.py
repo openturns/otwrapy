7a8f8753c96608c5bd4fc7d9c74e4f4b9d1c2fa8
Julien Schueller <schueller@phimeca.com>
2018-10-01 16:44:23 +0200

    Set version number 0.8

--- otwrapy/__init__.py
+++ otwrapy/__init__.py
@@ -8,8 +8,8 @@ General purpose OpenTURNS python wrapper tools
 import os
 
 __author__ = "Felipe Aguirre Martinez"
-__copyright__ = "Copyright 2017, Phimeca Engineering"
-__version__ = "0.7"
+__copyright__ = "Copyright 2018 Phimeca Engineering"
+__version__ = "0.8"
 __email__ = "aguirre@phimeca.fr"
 
 base_dir = os.path.dirname(__file__)
